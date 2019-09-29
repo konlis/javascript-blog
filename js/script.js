8eb63ce4da99cb95f0533b2ce98eb43b1d6b0919
Konrad Lis <o.konradlis@gmail.com>
2019-09-29 14:15:49 +0200

    Add NEW instructon to 'generarteTags'

--- js/script.js
+++ js/script.js
@@ -70,6 +70,9 @@ function generateTitleLinks(customSelector = ' ') {
 generateTitleLinks();
 
 function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   //console.log('articles', articles);
@@ -95,12 +98,22 @@ function generateTags() {
       /* add generated code to html variable */
       html = html + linkHTML;
       //console.log('html', html);
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
       /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
     taglist.innerHTML = html;
     /* END LOOP: for every article: */
   }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 generateTags();
 
